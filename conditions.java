4b9756b46fd2734a777b2be783d65d910a3eec3c
MuskanEzaz <143068774+MuskanEzaz@users.noreply.github.com>
2023-10-24 10:32:00 +0530

    Add files via upload

---
+++ conditions.java
@@ -0,0 +1,38 @@
+// import java.util.Scanner;
+
+// public class conditions{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         if(a==b){
+//             System.out.println("Eqal");
+           
+//         } else if(a>b){
+//           System.out.println("a is greater");  
+//         } else{
+//             System.out.println("a is lesser");
+//         }
+//     }
+// }
+
+
+
+import java.util.Scanner;
+public class conditions{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a==1){
+            System.out.println("hello");
+        } else if (a==2){
+           System.out.println("namaste"); 
+        } else{
+           System.out.println("bonjour"); 
+        }
+
+    }
+}
+
